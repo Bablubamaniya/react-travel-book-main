4699a46d2eecf1f7e4a93883d46d68fa9f5674a2
Bablu Bamaniya <bamaniyabablu428@gmail.com>
2025-01-11 21:52:58 +0530

    sideBar done

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+import { NavLink } from "react-router";
+import styles from "../styles/appNav.module.css";
+
+function AppNav() {
+    return (
+        <nav className={styles.nav}>
+            <ul>
+                <li className={styles.active}>
+                    <NavLink to="cities">Cities</NavLink>
+                </li>
+                <li>
+                    <NavLink to="countries">Countries</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default AppNav;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,20 @@
 // import React from 'react'
-
+import style from "../styles/SideBar.module.css";
+import LogoBox from "./LogoBox";
+import AppNav from "./AppNav";
+import City from "./City";
+import Footer from "./Footer";
 function SideBar() {
-  return (
-    <div>SideBar</div>
-  )
+    return (
+        <div className={style.sideBar}>
+            <LogoBox />
+            <AppNav />
+            {/* <CityList /> */}
+            <City />
+            {/* <AddCityForm /> */}
+            <Footer />
+        </div>
+    );
 }
 
-export default SideBar
+export default SideBar;
