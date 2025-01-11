e6c3b8d7b4b9ebe1801d80277c225353ac5cd048
Bablu Bamaniya <bamaniyabablu428@gmail.com>
2025-01-11 21:32:58 +0530

    App page done

--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -1,5 +1,11 @@
+import SideBar from "../components/SideBar";
+import Map from "../components/Map";
+import style from "../styles/AppPage.module.css"
 function AppPage() {
-  return <div>AppPage</div>;
+  return <div className={style.appPage}>
+  <SideBar />
+  <Map />
+  </div>;
 }
 
 export default AppPage;
